882d44822071b1c72b8aaa69770688c15cd8a332
JMCharter <JMCharter@gmail.com>
2014-12-08 15:28:14 -0500

    Added bootstrap and active navbar

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route("/")
+def test():
 
-def hello():
-    return "Hello, I love Digital Ocean!"
+    title = "FlaskApp"
+    paragraph = "Welcome to this Flask Application"
+    activePage = "index"
+
+    return render_template(
+        "index.html", paragraph=paragraph, title=title, activePage=activePage)
+
+
+@app.route("/about")
+def about():
+
+    title = "About"
+    paragraph = "How's this for an about page?"
+    activePage = "about"
+
+    return render_template(
+        "index.html", paragraph=paragraph, title=title, activePage=activePage)
+
+
+@app.route("/contact")
+def contact():
+
+    title = "Contact"
+    paragraph = "How's this for a contact page?"
+    activePage = "contact"
+
+    return render_template(
+        "index.html", paragraph=paragraph, title=title, activePage=activePage)
+
+
+@app.route("/form")
+def form():
+
+    pageType = "form"
+    title = "Form"
+    paragraph = "How's this for a form?"
+    activePage = "form"
+
+    return render_template(
+        "index.html",
+        paragraph=paragraph,
+        title=title,
+        pageType=pageType,
+        activePage=activePage)
+
 
 if __name__ == "__main__":
     app.run()
